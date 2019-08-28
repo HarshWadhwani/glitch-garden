632085ac2f117cb4024b64d6a93c9094daa34879
Wadhwani <Harsh.Wadhwani@ttu.edu>
2019-08-28 16:35:38 -0500

    adding files to repository

---
+++ Glitch Garden/Assets/scripts/AttackerSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker[] attackerPrefab;
+
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while(spawn)
+        {
+
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    public void StopSpawning()
+    {
+        spawn = false;
+    }
+
+    private void SpawnAttacker()
+    {
+        var attackerIndex = Random.Range(0, attackerPrefab.Length);
+        Spawn(attackerPrefab[attackerIndex]);
+    }
+
+    private void Spawn(Attacker myAttacker)
+    {
+        Attacker newAttacker = Instantiate(myAttacker, transform.position, transform.rotation) as Attacker;
+        newAttacker.transform.parent = transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Glitch Garden/Assets/scripts/Lives.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour
+{
+    [SerializeField] int damage = 1;
+    [SerializeField] float baseLives = 3;
+    float lives;
+    Text livesText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = baseLives - PlayerPrefsController.GetDifficulty();
+        livesText = GetComponent<Text>();
+        UpdateDisplay();
+        Debug.Log("difficulty setting currently is " + PlayerPrefsController.GetDifficulty());
+    }
+
+    private void UpdateDisplay()
+    {
+        livesText.text = lives.ToString();
+    }
+
+    public void TakeLife()
+    {
+        lives -= damage;
+        UpdateDisplay();
+
+        if (lives <= 0)
+        {
+            //FindObjectOfType<LevelLoader>().LoadYouLose();
+            FindObjectOfType<LevelController>().HandleLoseCondition();
+        }
+    }
+}
